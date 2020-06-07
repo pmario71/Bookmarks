b7d75faf48d198e7b3d87192260929acccf96c48
Mario Plendl <mario.plendl@outlook.com>
2020-06-07 17:01:24 +0200

    Added validation

---
+++ src/BookmarksWebApp/Pages/ServerSideValidator.cs
@@ -0,0 +1,73 @@
+﻿using Bookmarks.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookmarks.Pages
+{
+    public class ServerSideValidator : ComponentBase
+    {
+        private ValidationMessageStore _messageStore;
+
+        [CascadingParameter] EditContext CurrentEditContext { get; set; }
+
+        /// <inheritdoc />
+        protected override void OnInitialized()
+        {
+            if (CurrentEditContext == null)
+            {
+                throw new InvalidOperationException($"{nameof(ServerSideValidator)} requires a cascading " +
+                    $"parameter of type {nameof(EditContext)}. For example, you can use {nameof(ServerSideValidator)} " +
+                    $"inside an {nameof(EditForm)}.");
+            }
+
+            _messageStore = new ValidationMessageStore(CurrentEditContext);
+            CurrentEditContext.OnValidationRequested += (s, e) => 
+            {
+                Console.WriteLine("Validation requested ...");
+                _messageStore.Clear();
+                var model = CurrentEditContext.Model as BookmarkSvc;
+
+                try
+                {
+                    model.ApplyFilter();
+                }
+                catch(ArgumentOutOfRangeException ex)
+                {
+                    _messageStore.Add(CurrentEditContext.Field(nameof(BookmarkSvc.Filter)), ex.Message);
+                }
+                CurrentEditContext.NotifyValidationStateChanged();
+            };
+
+            CurrentEditContext.OnFieldChanged += (s, e) => 
+            { 
+                _messageStore.Clear(e.FieldIdentifier);
+
+                var model = CurrentEditContext.Model as BookmarkSvc;
+
+                try
+                {
+                    model.ApplyFilter();
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    _messageStore.Add(CurrentEditContext.Field(nameof(BookmarkSvc.Filter)), ex.Message);
+                }
+                CurrentEditContext.NotifyValidationStateChanged();
+            };
+        }
+
+        public void DisplayErrors(Dictionary<string, List<string>> errors)
+        {
+            foreach (var err in errors)
+            {
+                _messageStore.Add(CurrentEditContext.Field(err.Key), err.Value);
+            }
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+    }
+
+}
--- src/BookmarksWebApp/Services/BookmarkSvc.cs
+++ src/BookmarksWebApp/Services/BookmarkSvc.cs
@@ -1,6 +1,8 @@
 ﻿using Bookmarks.DataModel;
+using Bookmarks.DataModel.Filtering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
@@ -10,8 +12,10 @@ namespace Bookmarks.Services
 {
     public class BookmarkSvc
     {
+        private static BookmarkFilter _defaultEmptyFilterComponent = BookmarkFilter.Create(string.Empty);
         private Configuration _configuration;
         private readonly HttpClient _httpClient;
+        private BookmarkFilter _filterComponent = _defaultEmptyFilterComponent;
 
         public BookmarkSvc(HttpClient httpClient)
         {
@@ -24,6 +28,27 @@ namespace Bookmarks.Services
             {
                 _configuration = await _httpClient.GetFromJsonAsync<Configuration>("sample-data/bookmarks.json");
             }
+        }
+
+        public string Filter { get; set; }
+
+        public IEnumerable<Bookmark> FilteredBookmarks
+        {
+            get
+            {
+                return Config.Bookmarks.Where(bm => _filterComponent.FilterOnSearchString(bm));
+            }
+        }
+
+        public void ApplyFilter()
+        {
+            _filterComponent = BookmarkFilter.Create(this.Filter);
+        }
+
+        public void ResetFilter()
+        {
+            this.Filter = string.Empty;
+            _filterComponent = _defaultEmptyFilterComponent;
         }
 
         public Configuration Config => _configuration;
